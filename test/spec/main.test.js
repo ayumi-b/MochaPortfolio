f19bffbe397cbed4de7c9e1ed061200dd04330c9
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-02-05 12:09:21 -0600

    added more tests

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -6,3 +6,33 @@ describe('test suite', function (){
     false.should.be.false;
   });
 });
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});
+
+describe('getStock', function() {
+  it('should return a stock object', function (done) {
+    getStock('AAPL', function (stock) {
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+  }); 
+  it('should return a stock object', function (done) {
+    getStock('MSFT', function (stock) {
+      stock.Name.should.equal('Microsoft Corp');
+      done();
+    });
+  });
+});
+
+describe('addStockToTable', function () {
+  it('should get the stock info and append the table', function (){
+    var stock = { Name: "SuperCorp", Symbol: "SCRP", LastPrice: 12.34 }
+    $('tr').length.should.equal(0);
+    addStockToTable(stock);
+    $('tr').length.should.equal(1);
+  });
+});

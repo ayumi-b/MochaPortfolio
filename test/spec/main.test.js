9809f28048b4538d44bab77fba8461ba50330a24
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-02-05 16:10:26 -0600

    added async function/test

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,4 +1,5 @@
-/* jshint mocha: true, expr: true, strict: false */
+/* jshint jquery: true */
+/* global async: false */
 
 describe('test suite', function (){
   it('should assert true', function () {
@@ -40,9 +41,39 @@ describe('addStockToTable', function () {
         $row  = addStockToTable(stock);
         $tds  = $row.find('td');
 
-    $tds.length.should.equal(3);
+    $tds.length.should.equal(4);
     $($tds[0]).text().should.equal('SuperCorp');
     $($tds[1]).text().should.equal('SCRP');
     $($tds[2]).text().should.equal('12.34');
+    $($tds[3]).text().should.equal('12.34');
+
+  });
+});
+
+describe('getMultipleStocks', function (){
+  it('should return multiple stock objects', function (done){
+    getMultipleStocks(['AAPL', 'MSFT'], function (stocks){
+      stocks.length.should.equal(2);
+      stocks[0].Name.should.equal('Apple Inc');
+      stocks[1].Name.should.equal('Microsoft Corp');
+      done();
+    });
+  });
+});
+
+describe('refreshStockPrices', function (){
+  it('should edit each stock in the table with a new price', function () {
+    var stocks = [
+      {Symbol: 'AAPL', LastPrice: 12.45}, 
+      {Symbol: 'MSFT', LastPrice: 23.56}
+    ];
+
+    $('tbody').append('<tr><td>Apple Inc</td><td>AAPL</td><td>12.34</td><td>12.34</td></tr>');
+    $('tbody').append('<tr><td>Microsoft Corp</td><td>MSFT</td><td>12.34</td><td>12.34</td></tr>');
+ 
+    refreshStockPrices(stocks);
+
+    $($($trs[0]).find('td')[3]).text().should.equal('12.45');
+    $($($trs[1]).find('td')[3]).text().should.equal('23.56');
   });
 });
